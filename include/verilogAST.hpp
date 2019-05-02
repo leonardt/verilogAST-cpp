5380c330cd8ea5dace8ecbcc24c80f5ab2a51361
Lenny Truong <lenny@stanford.edu>
2019-05-02 14:34:21 -0700

    Move toString to cpp file

--- include/verilogAST.hpp
+++ include/verilogAST.hpp
@@ -41,30 +41,7 @@ public:
 
   NumericLiteral(std::string value)
       : value(value), size(32), _signed(false), radix(decimal){};
-
-  std::string toString() {
-    if (_signed) {
-      throw std::runtime_error(
-          "NumericLiteral::toString when _signed == True has "
-          "not been implemented");
-    }
-    char radix_str;
-    switch (radix) {
-    case binary:
-      radix_str = 'b';
-      break;
-    case octal:
-      radix_str = 'o';
-      break;
-    case hex:
-      radix_str = 'h';
-      break;
-    case decimal:
-      radix_str = 'd';
-      break;
-    }
-    return std::to_string(size) + "'" + radix_str + value;
-  };
+  std::string toString();
 };
 
 class Identifier : Expression {
@@ -72,7 +49,7 @@ class Identifier : Expression {
 
 public:
   Identifier(std::string value) : value(value){};
-  std::string toString() { return value; };
+  std::string toString();
 };
 
 class Index : Expression {
@@ -81,9 +58,7 @@ class Index : Expression {
 
 public:
   Index(Identifier *id, NumericLiteral *index) : id(id), index(index){};
-  std::string toString() {
-    return id->toString() + '[' + index->toString() + ']';
-  };
+  std::string toString();
 };
 
 class Slice : Expression {
--- src/verilogAST.cpp
+++ src/verilogAST.cpp
@@ -1,1 +1,32 @@
 #include "verilogAST.hpp"
+namespace verilogAST {
+std::string NumericLiteral::toString() {
+  if (_signed) {
+    throw std::runtime_error(
+        "NumericLiteral::toString when _signed == True has "
+        "not been implemented");
+  }
+  char radix_str;
+  switch (radix) {
+  case binary:
+    radix_str = 'b';
+    break;
+  case octal:
+    radix_str = 'o';
+    break;
+  case hex:
+    radix_str = 'h';
+    break;
+  case decimal:
+    radix_str = 'd';
+    break;
+  }
+  return std::to_string(size) + "'" + radix_str + value;
+};
+
+std::string Identifier::toString() { return value; };
+
+std::string Index::toString() {
+  return id->toString() + '[' + index->toString() + ']';
+};
+}; // namespace verilogAST
