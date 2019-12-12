39279230562ac8bdc374de9b11522dbb42b16df8
Lenny Truong <leonardtruong@fb.com>
2019-12-11 16:38:25 -0800

    Add transformer test

---
+++ tests/transformer.cpp
@@ -0,0 +1,52 @@
+#include "gtest/gtest.h"
+#include "verilogAST.hpp"
+
+namespace vAST = verilogAST;
+
+class XtoZ : public vAST::Transformer {
+  public:
+  using vAST::Transformer::visit;
+  virtual std::unique_ptr<vAST::Expression> visit(
+      std::unique_ptr<vAST::Identifier> node) {
+    if (node->value == "x") {
+      return vAST::make_id("z");
+    }
+    return node;
+  };
+};
+
+class ReplaceNameWithExpr : public vAST::Transformer {
+  public:
+  using vAST::Transformer::visit;
+  virtual std::unique_ptr<vAST::Expression> visit(
+      std::unique_ptr<vAST::Identifier> node) {
+    if (node->value == "x") {
+      return vAST::make_binop(
+              vAST::make_id("z"),
+              vAST::BinOp::SUB,
+              vAST::make_id("w")
+      );
+    }
+    return node;
+  };
+};
+
+namespace {
+TEST(TransformerTests, TestXtoZ) {
+  std::unique_ptr<vAST::Expression> expr = vAST::make_binop(
+      vAST::make_id("x"), vAST::BinOp::ADD, vAST::make_id("y"));
+  XtoZ transformer;
+  EXPECT_EQ(transformer.visit(std::move(expr))->toString(), "z + y");
+}
+TEST(TransformerTests, TestReplaceNameWithExpr) {
+  std::unique_ptr<vAST::Expression> expr = vAST::make_binop(
+      vAST::make_id("x"), vAST::BinOp::MUL, vAST::make_id("y"));
+  ReplaceNameWithExpr transformer;
+  EXPECT_EQ(transformer.visit(std::move(expr))->toString(), "(z - w) * y");
+}
+}  // namespace
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
