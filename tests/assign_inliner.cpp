268cb716b9970abd4002b192c72852f28345dde5
Lenny Truong <leonardtruong@fb.com>
2019-12-16 11:53:25 -0800

    Add test file

---
+++ tests/assign_inliner.cpp
@@ -0,0 +1,50 @@
+#include "common.cpp"
+#include "gtest/gtest.h"
+#include "verilogAST/transformer.hpp"
+
+namespace vAST = verilogAST;
+
+class AssignInliner : public vAST::Transformer {
+ public:
+  using vAST::Transformer::visit;
+};
+
+namespace {
+TEST(InlineAssignTests, TestBasic) {
+  std::vector<std::unique_ptr<vAST::AbstractPort>> ports;
+  ports.push_back(std::make_unique<vAST::Port>(vAST::make_id("i"), vAST::INPUT,
+                                               vAST::WIRE));
+  ports.push_back(std::make_unique<vAST::Port>(vAST::make_id("o"), vAST::OUTPUT,
+                                               vAST::WIRE));
+
+  std::vector<std::variant<std::unique_ptr<vAST::StructuralStatement>,
+                           std::unique_ptr<vAST::Declaration>>>
+      body;
+
+  body.push_back(
+      std::make_unique<vAST::Wire>(std::make_unique<vAST::Identifier>("x")));
+
+  body.push_back(std::make_unique<vAST::ContinuousAssign>(
+      std::make_unique<vAST::Identifier>("x"),
+      std::make_unique<vAST::Identifier>("i")));
+
+  body.push_back(std::make_unique<vAST::ContinuousAssign>(
+      std::make_unique<vAST::Identifier>("o"),
+      std::make_unique<vAST::Identifier>("x")));
+
+  std::unique_ptr<vAST::AbstractModule> module = std::make_unique<vAST::Module>(
+      "test_module", std::move(ports), std::move(body));
+  std::string expected_str =
+      "module test_module (input i, output o);\n"
+      "assign o = i;\n"
+      "endmodule\n";
+
+  vAST::AssignInliner transformer;
+  EXPECT_EQ(transformer.visit(std::move(module))->toString(), expected_str);
+}
+}  // namespace
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
