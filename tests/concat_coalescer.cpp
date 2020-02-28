13785a0efbcaecc179fe48095b0e9744b18fd18d
Rajsekhar Setaluri <rajsekhar@fb.com>
2020-02-28 14:52:50 -0800

    Add test for multiple names in single concat

--- tests/concat_coalescer.cpp
+++ tests/concat_coalescer.cpp
@@ -122,8 +122,63 @@ TEST(ConcatCoalescerTests, ReverseRun) {
       ");\n"
       "assign O = {I[4],I[5],I[6],I[7],I[3:0]};\n"
       "endmodule\n";
-  // Sequence has no contiguous runs.
   runTest({0, 1, 2, 3, 7, 6, 5, 4}, pre, post);
+}
+
+TEST(ConcatCoalescerTests, MultipleNames) {
+  auto pre =
+      "module test_module (\n"
+      "    input [7:0] I0,\n"
+      "    input [7:0] I1,\n"
+      "    output [7:0] O\n"
+      ");\n"
+      "assign O = {I0[7],I0[6],I0[5],I0[4],I1[3],I1[2],I1[1],I1[0]};\n"
+      "endmodule\n";
+  auto post =
+      "module test_module (\n"
+      "    input [7:0] I0,\n"
+      "    input [7:0] I1,\n"
+      "    output [7:0] O\n"
+      ");\n"
+      "assign O = {I0[7:4],I1[3:0]};\n"
+      "endmodule\n";
+  std::vector<std::unique_ptr<vAST::AbstractPort>> ports;
+  ports.push_back(std::make_unique<vAST::Port>(makeVector("I0", 7, 0),
+                                               vAST::INPUT,
+                                               vAST::WIRE));
+  ports.push_back(std::make_unique<vAST::Port>(makeVector("I1", 7, 0),
+                                               vAST::INPUT,
+                                               vAST::WIRE));
+  ports.push_back(std::make_unique<vAST::Port>(makeVector("O", 7, 0),
+                                               vAST::OUTPUT,
+                                               vAST::WIRE));
+  std::vector<BodyElement> body;
+
+  std::vector<std::unique_ptr<vAST::Expression>> args = {};
+  for (int i = 7; i >= 0; i--) {
+    auto id = i > 3 ? "I0" : "I1";
+    args.emplace_back(
+        new vAST::Index(
+            vAST::make_id(id),
+            vAST::make_num(std::to_string(i))));
+  }
+  std::unique_ptr<vAST::Expression> rhs(new vAST::Concat(std::move(args)));
+
+  body.push_back(std::make_unique<vAST::ContinuousAssign>(
+      std::make_unique<vAST::Identifier>("O"), std::move(rhs)));
+
+  std::unique_ptr<vAST::AbstractModule> module = std::make_unique<vAST::Module>(
+      "test_module",
+      std::move(ports),
+      std::move(body));
+
+  EXPECT_EQ(module->toString(), pre);
+
+  // Run ConcatCoalescer transformer.
+  vAST::ConcatCoalescer transformer;
+  module = transformer.visit(std::move(module));
+
+  EXPECT_EQ(module->toString(), post);
 }
 
 }  // namespace
